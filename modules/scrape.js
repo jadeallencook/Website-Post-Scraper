820671dda05f8d9059a75ad11986cb2d5c69803b
Jade Allen Cook <jadeallencook@gmail.com>
2018-09-25 09:34:50 -0600

    ⏰ Added elapse time read out

--- modules/scrape.js
+++ modules/scrape.js
@@ -1,6 +1,7 @@
 const host = require('./host'),
     links = require('./links'),
     backup = require('./backup'),
+    time = require('./time'),
     print = require('./print');
 
 module.exports = {
@@ -9,6 +10,7 @@ module.exports = {
             link = host.url + href;
         host.links.cache.splice(0, 1);
         host.links.queued = host.links.cache.length;
+        var start = Date.now();
         casper.thenOpen(link, function () {
             links.cache(this.evaluate(links.extract));
             host.links.visited.push(href);
@@ -18,7 +20,12 @@ module.exports = {
             backup.now(host.links);
             if (host.links.queued > 0) {
                 this.links(casper);
-                print.message(host.links.queued + ' links left \n', host.links.cache.length, 'yellow');
+                var end = Date.now(),
+                    average = time.duration(start, end);
+                time.times.push(average);
+                average = time.average();
+                print.message(host.links.queued + ' links left', host.links.cache.length, 'yellow');
+                print.message('~' + average + ' min(s) \n', '~' + average, 'blue');
             } else {
                 host.links.queued = null;
                 print.message('complete\n', 'complete', 'green');
---
+++ modules/time.js
@@ -0,0 +1,18 @@
+var host = require('./host');
+
+module.exports = {
+    duration: function (start, end) {
+        return end - start;
+    },
+    average: function () {
+        var minutes = this.times.reduce(function (a, b) {
+            return a + b;
+        }, 0)
+        minutes = minutes / this.times.length;
+        minutes = host.links.queued * minutes;
+        minutes = minutes / 1000;
+        minutes = Math.floor(minutes / 60);
+        return minutes;
+    },
+    times: []
+}
