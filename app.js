c5e289a525c15de377de618434a2bccd6fc1c397
Jade Allen Cook <jadeallencook@gmail.com>
2018-09-25 03:35:13 -0600

    🔨 Refactored scraping functionality

--- app.js
+++ app.js
@@ -23,7 +23,29 @@
     backup.filename = host.name();
     links.import();
 
+    function scrape() {
+        const href = host.links.cache[0],
+            link = host.url + href;
+        casper.thenOpen(link, function () {
+            links.cache(this.evaluate(links.extract));
+            host.links.visited.push(href);
+            host.links.cache.splice(host.links.cache.indexOf(href), 1);
+            print.message(link);
+            print.message(host.links.products.length + ' pages saved', host.links.products.length, 'green');
+        }).then(function () {
+            host.links.queued = host.links.cache.length;
+            backup.now(host.links);
+            if (host.links.queued > 0) {
+                print.message(host.links.queued + ' links left \n', host.links.cache.length, 'yellow');
+                scrape();
+            } else {
+                print.message('\ncould not find any more links, complete!\n');
+            }
+        });
+    }
+
     // init
+    print.message('loading...\n', 'loading', 'yellow');
     casper.start(host.url).then(function () {
         print.message('successfully connected\n', 'successfully', 'green');
         links.cache(this.evaluate(links.extract));
@@ -31,28 +53,7 @@
             print.message('no more links found\n', 'no more links found', 'yellow');
         } else {
             backup.now();
-            for (var x = 0, max = host.links.cache.length; x < max; x++) {
-                var link = host.links.cache[x];
-                var href = link;
-                link = host.url + href;
-                casper.thenOpen(link, function () {
-                    links.cache(this.evaluate(links.extract));
-                    host.links.visited.push(href);
-                    host.links.cache.splice(host.links.cache.indexOf(href), 1);
-                    print.message(link);
-                    print.message(host.links.products.length + ' pages saved', host.links.products.length, 'green');
-                }).then(function () {
-                    host.links.queued = host.links.cache.length;
-                    backup.now(host.links);
-                    print.message(host.links.cache.length + ' links left \n', host.links.cache.length, 'yellow');
-                    if (host.links.queued === 0) {
-                        print.message('could not find any more links, complete!\n');
-                        if (host.links.cache.length > 0) {
-                            scrapeCachedLinks();
-                        }
-                    }
-                });
-            }
+            scrape();
         }
     }).run();
 })();
